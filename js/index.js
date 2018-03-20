dc4478e09e84d96548922222f560d87a5560ee16
Anand Soni <anandsoni.india@gmail.com>
2018-03-20 14:31:15 +0530

    Pushing with Vue

--- js/index.js
+++ js/index.js
@@ -1,9 +1,58 @@
 //jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
+var current_fs, next_fs, nextLog, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
+var BaseURL = "http://192.168.0.103:3000";
 
-$(".next").click(function() {
+$(document).ready(function () {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", BaseURL + "/getTransaction", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(JSON.stringify({ phoneNumber: "1234567890" }));
+  var response = JSON.parse(xhttp.responseText);
+  if (response.items) {
+    for (i = 0; i < response.items.length; i++) {
+      if (response.items.tokenName != "MNY") {
+        if (response.items.status != "freezed") {
+          $('.fs-table').append("<div><tb><h3 class=\"tokenName\">" + response.items.balance + " " + response.items.tokenName + "</h3><button type=\"submit\" id=\"ClaimSub\" name=\"submit\" class=\"action-button\" value=\"CLAIM\" /></div></tb>");
+        } else {
+          $('.fs-table').append("<div><tb><h3 class=\"tokenName\">" + response.items.tokenName + "</h3><h2>Freezed</h2></div></tb>");
+        }
+      }
+    }
+  }
+  console.log(response.items);
+});
+
+$("#login").click(function () {
+  var xhttp = new XMLHttpRequest();
+  var phone = (document.getElementById('phoneNumber').value);
+  console.log(phone);
+  xhttp.open("POST", BaseURL + "/login", false);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(JSON.stringify({ mobileNumber: phone }));
+  // xhttp.send();
+  var response = JSON.parse(xhttp.responseText);
+  console.log(response);
+  if (response.id) {
+    sessionStorage.setItem("mobile", phone);
+    nextLog = $(".nextLog")
+      .parent()
+      .next();
+
+    xhttp.open("POST", BaseURL + "/getTransaction", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({ phoneNumber: sessionStorage.getItem("mobile") }));
+    var response = JSON.parse(xhttp.responseText);
+    console.log(response);
+
+  } else {
+    alert("This user is not registered with us");
+  }
+
+});
+
+$(".next").click(function () {
   if (animating) return false;
   animating = true;
 
@@ -23,7 +72,7 @@ $(".next").click(function() {
   current_fs.animate(
     { opacity: 0 },
     {
-      step: function(now, mx) {
+      step: function (now, mx) {
         //as the opacity of current_fs reduces to 0 - stored in "now"
         //1. scale current_fs down to 80%
         scale = 1 - (1 - now) * 0.2;
@@ -38,7 +87,7 @@ $(".next").click(function() {
         next_fs.css({ left: left, opacity: opacity });
       },
       duration: 800,
-      complete: function() {
+      complete: function () {
         current_fs.hide();
         animating = false;
       },
@@ -48,7 +97,7 @@ $(".next").click(function() {
   );
 });
 
-$(".previous").click(function() {
+$(".previous").click(function () {
   if (animating) return false;
   animating = true;
 
@@ -68,7 +117,7 @@ $(".previous").click(function() {
   current_fs.animate(
     { opacity: 0 },
     {
-      step: function(now, mx) {
+      step: function (now, mx) {
         //as the opacity of current_fs reduces to 0 - stored in "now"
         //1. scale previous_fs from 80% to 100%
         scale = 0.8 + (1 - now) * 0.2;
@@ -83,7 +132,7 @@ $(".previous").click(function() {
         });
       },
       duration: 800,
-      complete: function() {
+      complete: function () {
         current_fs.hide();
         animating = false;
       },
@@ -93,15 +142,10 @@ $(".previous").click(function() {
   );
 });
 
-$(".submit").click(function() {
-  var count = 1;
-  var property = document.getElementById(".sub");
-  if (count == 0) {
-            property.style.backgroundColor = "#FFFFFF"
-            count = 1;        
-        }
-        else {
-            property.style.backgroundColor = "#7FFF00"
-            count = 0;
-        }
+$("#ClaimSub").click(function () {
+  xhttp.open("POST", BaseURL + "/getTransaction", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+  xhttp.send(JSON.stringify({ phoneNumber: sessionStorage.getItem("mobile") }));
+  var response = JSON.parse(xhttp.responseText);
+  console.log(response);
 });
